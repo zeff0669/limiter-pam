a4ef2258b5dd55b9d2276564fa4229c023c4bd20
ZEFF <evandrogoetz5@gmail.com>
2024-04-24 17:14:10 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -5,7 +5,7 @@ chmod +x /usr/local/bin/limiter-pam
 if [ -f /usr/local/bin/limiter-pam ]; then
     #echo "auth required pam_exec.so /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
     echo "account required pam_exec.so quiet /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
-    echo "account required pam_echo.so LIMITE DE CONEXÕES ATINGIDO" >> /etc/pam.d/sshd
+    echo "account required pam_echo.so LIMITE DE CONEXÕES DE $$PAM_USER ATINGIDO" >> /etc/pam.d/sshd
     echo "ClientAliveInterval 2" >> /etc/ssh/sshd_config
     echo "ClientAliveCountMax 1" >> /etc/ssh/sshd_config
     systemctl restart ssh
