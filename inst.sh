af5d9f4edd3a1b78f19b5ec73d750a08a0fa47de
ZEFF <evandrogoetz5@gmail.com>
2024-04-24 11:34:39 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+wget -O /usr/local/bin/check_login_sessions.sh https://github.com/zeff0669/limiter-pam/raw/main/check_login_sessions.sh
+chmod +x /usr/local/bin/check_login_sessions.sh
 
-#wget 
-
-
-echo "auth required pam_exec.so /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
+if [ -f /usr/local/bin/check_login_sessions.sh ]; then
+    echo "auth required pam_exec.so /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
+    echo "ClientAliveInterval 2" >> /etc/ssh/sshd_config
+    echo "ClientAliveCountMax 1" >> /etc/ssh/sshd_config
+    systemctl restart ssh
+fi
