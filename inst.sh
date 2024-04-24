c1b40dee8b038b8898a6d60c951737ecaaba44ad
ZEFF <evandrogoetz5@gmail.com>
2024-04-24 19:21:06 -0300

    Atualizar o inst.sh

--- inst.sh
+++ inst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Author: https://github.com/zeff0669
-wget -O /usr/local/bin/limiter-pam https://github.com/zeff0669/limiter-pam/raw/main/limiter-pam
-chmod +x /usr/local/bin/limiter-pam
+wget -O /usr/local/bin/limiter-pam https://github.com/zeff0669/limiter-pam/raw/main/limiter-pam >/dev/null 2>&1
+chmod +x /usr/local/bin/limiter-pam >/dev/null 2>&1
 
 if [ -f /usr/local/bin/limiter-pam ]; then
     #echo "auth required pam_exec.so /usr/local/bin/limiter-pam" >> /etc/pam.d/sshd
@@ -9,5 +9,5 @@ if [ -f /usr/local/bin/limiter-pam ]; then
     echo "account required pam_echo.so LIMITE DE CONEXÕES DE ATINGIDO" >> /etc/pam.d/sshd
     echo "ClientAliveInterval 2" >> /etc/ssh/sshd_config
     echo "ClientAliveCountMax 1" >> /etc/ssh/sshd_config
-    systemctl restart ssh
+    systemctl restart ssh >/dev/null 2>&1
 fi
