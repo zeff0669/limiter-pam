e8e395d6296afb4b9daf535e6d755a9469aab4db
ZEFF <evandrogoetz5@gmail.com>
2024-04-24 17:18:07 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -3,8 +3,8 @@ wget -O /usr/local/bin/limiter-pam https://github.com/zeff0669/limiter-pam/raw/m
 chmod +x /usr/local/bin/limiter-pam
 
 if [ -f /usr/local/bin/limiter-pam ]; then
-    #echo "auth required pam_exec.so /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
-    echo "account required pam_exec.so quiet /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
+    #echo "auth required pam_exec.so /usr/local/bin/limiter-pam" >> /etc/pam.d/sshd
+    echo "account required pam_exec.so quiet /usr/local/bin/limiter-pam" >> /etc/pam.d/sshd
     echo "account required pam_echo.so LIMITE DE CONEXÕES DE ATINGIDO" >> /etc/pam.d/sshd
     echo "ClientAliveInterval 2" >> /etc/ssh/sshd_config
     echo "ClientAliveCountMax 1" >> /etc/ssh/sshd_config
