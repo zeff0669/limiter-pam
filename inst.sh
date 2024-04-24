8c6a6b6cd067ebd273fd4c74124db4fa7ada094d
ZEFF <evandrogoetz5@gmail.com>
2024-04-24 16:45:12 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -3,7 +3,9 @@ wget -O /usr/local/bin/check_login_sessions.sh https://github.com/zeff0669/limit
 chmod +x /usr/local/bin/check_login_sessions.sh
 
 if [ -f /usr/local/bin/check_login_sessions.sh ]; then
-    echo "auth required pam_exec.so /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
+    #echo "auth required pam_exec.so /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
+    echo "account required pam_exec.so quiet /usr/local/bin/check_login_sessions.sh" >> /etc/pam.d/sshd
+    echo "account required pam_echo.so LIMITE DE CONEXÕES ATINGIDO" >> /etc/pam.d/sshd
     echo "ClientAliveInterval 2" >> /etc/ssh/sshd_config
     echo "ClientAliveCountMax 1" >> /etc/ssh/sshd_config
     systemctl restart ssh
